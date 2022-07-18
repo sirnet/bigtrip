9627ec3d5fd4c0295b4c73a86893add3abf51f08
unknown <artemshomin@gmail.com>
2022-07-18 07:13:55 +0300

    подготовка к 2-у заданию

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import { createMenuTemplate } from "./view/menu";
 import { createRouteInfoTemplate } from "./view/route-info";
 import { createFilterTemplate } from "./view/filters";
-import { createSortTemplate } from "./view/sort";
+import { createTripBoardTemplate } from "./view/trip-board";
 import { createAddNewPoint } from "./view/add-new-point";
 import { createEditPointTemplate } from "./view/edit-point";
 import { createWaypointTemplate } from "./view/waypoint";
 
+const COUNT_POINT = 3;
 const render = (container, template, place) => {
     container.insertAdjacentHTML(place, template);
 };
 
 const siteBodyElement = document.querySelector('.page-body');
-render (siteBodyElement, createRouteInfoTemplate(), 'afterbegin');
+const tripRouteInfo = siteBodyElement.querySelector('.trip-main');
+
+render (tripRouteInfo, createRouteInfoTemplate(), 'afterbegin');
 
 const tripMenuElement = siteBodyElement.querySelector('.trip-controls__navigation');
 render (tripMenuElement, createMenuTemplate(), 'beforeend');
@@ -20,13 +23,12 @@ const tripFilterElement = siteBodyElement.querySelector('.trip-controls__filters
 render (tripFilterElement, createFilterTemplate(), 'beforeend');
 
 const tripBordElement = siteBodyElement.querySelector('.trip-events');
+render (tripBordElement, createTripBoardTemplate(), 'beforeend');
 
+const tripListElement = tripBordElement.querySelector('.trip-events__list');
+render (tripListElement, createAddNewPoint(), 'beforeend');
+render (tripListElement, createEditPointTemplate(), 'beforeend');
 
-
-
-
-
-render (tripEventsElement, createSortTemplate(), 'beforeend');
-render (tripEventsElement, createAddNewPoint(), 'beforeend');
-render (tripEventsElement, createEditPointTemplate(), 'beforeend');
-render (tripEventsElement, createWaypointTemplate(), 'beforeend');
+for (let i=0; i < COUNT_POINT; i++){
+    render (tripListElement, createWaypointTemplate(), 'beforeend');
+};
