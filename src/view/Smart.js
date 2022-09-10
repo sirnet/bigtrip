da15ef277bca3992f1ce42330bd7465eb3f7da95
sirnet <artemshomin@gmail.com>
2022-09-10 18:19:09 +0300

    Правки задания 6

---
+++ src/view/Smart.js
@@ -0,0 +1,17 @@
+import Abstract from "./abstract";
+
+export default class Smart extends Abstract {
+    constructor(){
+        super();
+        this._data = {};
+    }
+
+    updateElement() {
+
+    }
+
+    updateData() {
+        
+    }
+
+}
